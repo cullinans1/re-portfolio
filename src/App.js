bc5bc793d91bc54df63101e22bc01442d1650f04
Sammy <sammy.cullinan@gmail.com>
2021-03-19 16:44:27 -0700

    jumbotron fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
-import Page from "./components/Page";
+// import Page from "./components/Page";
 import "./App.css";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,9 +4,17 @@ function Header() {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="flex">
+        <div className="intro-text">
+          <h1 className=""> Hi, I am </h1>
+        </div>
+
         <h1 className="mytitle">Sammy Cullinan</h1>
+        <div className="intro-text">
+          <h4 className=" lead mytitle">And I am a</h4>
+        </div>
+
         <h4 className="lead mytitle"> Full-stack Web Developer</h4>
-        <a href="https://github.com/cullinans1" target="_blank">
+        <a href="https://github.com/cullinans1" target="_blank" rel="noopener noreferrer">
           <button className="btn btn-lg btn-outline-dark">
             <i className="fab fa-github fa-lg"></i>
           </button>
